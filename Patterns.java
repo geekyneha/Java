50d551a42d17d035c03b5a6b278b4bf1bf8259c2
geekyneha <nehatualea@gmail.com>
2025-07-27 00:21:26 +0530

    Pattern 7: Inverted Floyd's Triangle

--- Patterns.java
+++ Patterns.java
@@ -103,6 +103,21 @@ public class Patterns {
         System.out.println();
      }
 
+
+        // Pattern 7
+        // Inverted Floyd's Triangle
+        // 1 2 3 4
+        // 1 2 3
+        // 1 2
+        // 1
+    System.out.println("Pattern 7: Inverted Floyd's Triangle");
+     for(int i = 0 ; i < n  ; i++){
+        for(int j = 0 ;  j < n - i ; j++){
+             System.out.print( j + 1 + " ");
+        }
+        System.out.println();
+     }
+
     }
 
     
