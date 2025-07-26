5f8bdd3a710d62e99e30ab77ca83c734014cf679
geekyneha <nehatualea@gmail.com>
2025-07-27 00:53:36 +0530

    Pattern 9: 0-1 Pattern

--- Patterns.java
+++ Patterns.java
@@ -1,6 +1,6 @@
 public class Patterns {
     public static void main(String[] args){
-        int n = 4;
+        int n = 5;
         int m= 5;
         // Pattern 1
         // Solid Rectangle
@@ -118,6 +118,42 @@ public class Patterns {
         System.out.println();
      }
 
+        // Pattern 8
+        // Floyd's Triangle
+        // 1
+        // 2 3
+        // 4 5 6
+        // 7 8 9 10
+        // 11 12 13 14 15
+    System.out.println("Pattern 8: Floyd's Triangle");
+
+    int count = 0;
+    for(int i =  0; i < n ; i++){
+        for(int j = 0 ; j<= i  ; j++){
+           count++;
+           System.out.print(count + " ");
+        } 
+        System.out.println();
+    }
+
+    // pattern 9 : 0-1 Pattern;
+    // 1
+    // 0 1
+    // 1 0 1
+    // 0 1 0 1
+    System.out.println("Pattern 9: 0-1 Pattern");
+
+    for(int i = 1 ; i <= n ; i++){
+      for(int j = 1; j <= i ; j++){
+        if((i + j) % 2 == 0){
+            System.out.print(1 + " ");
+        }
+        else{
+            System.out.print(0 + " ");
+        }
+      }
+        System.out.println();
+    } 
     }
 
     
