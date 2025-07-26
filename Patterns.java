065a111ec06f82696e9438f5d0e02840ab70d0ca
geekyneha <nehatualea@gmail.com>
2025-07-26 23:44:37 +0530

    Pattern 3 : Half Pyramid

--- Patterns.java
+++ Patterns.java
@@ -37,6 +37,23 @@ public class Patterns {
              System.out.println();
         }
 
+        // Pattern 3
+    // Half Pyramid
+    // *
+    // * *
+    // * * *
+    // * * * *
+
+
+    System.out.println("Pattern 3: Half Pyramid");
+    for (int i = 0 ; i < n ; i++){
+        for (int j = 0 ; j <= i ; j++){
+            System.out.print("* ");
+        }
+        System.out.println();
     }
+
+    }
+
     
 }
