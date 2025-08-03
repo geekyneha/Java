8b42000496fbf1682457707f4f9148d75a940c7d
geekyneha <nehatualea@gmail.com>
2025-08-03 20:16:55 +0530

    Strings in Java

---
+++ Strings.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Strings {
+
+    public static void main(String args[]){
+
+
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter a string:");
+        String str = sc.nextLine();
+
+
+        for(int i =0; i < str.length(); i++){
+            System.out.println("Character at index " + i + ": " + str.charAt(i));
+        }
+
+        //string functions
+
+        // substring
+        System.out.println("Substring from index 2 to 5: " + str.substring(2, 5));
+
+    }
+        
+}
