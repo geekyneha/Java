8994f2ed0e004f3c7877b9449e9cbb4e6b609a6b
geekyneha <nehatualea@gmail.com>
2025-08-12 17:54:01 +0530

    Fibonacci series using recursion

---
+++ Fibo.java
@@ -0,0 +1,31 @@
+public class Fibo {
+
+
+
+    public static void fibo(int a,int b ,int n){
+          
+        if(n==0) return ;
+        
+    
+         int c = a + b;
+
+         System.out.println(c);
+
+         fibo(b,c,n - 1);
+
+
+
+    }
+
+    public static void main(String args[]){
+        
+        int n = 8;
+        int a = 0;
+        System.out.println(a);
+        int b= 1;
+        System.out.println(b);
+
+        fibo(a,b,n - 2);
+    }
+    
+}
