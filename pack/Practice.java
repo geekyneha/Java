9686c1a9a48e5d57f24f70643cfab99964020b02
geekyneha <nehatualea@gmail.com>
2025-08-27 22:36:40 +0530

    practice

--- pack/Practice.java
+++ pack/Practice.java
@@ -1,5 +1,10 @@
 package pack;
 
+// odd or even.
+// prime or not.
+//fibonacci series.
+
+
 public class Practice {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
